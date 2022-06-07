c68a66d64d9ae98e2b443076addcfd9986c251a5
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-07 23:53:39 +0100

    complete_profile good

--- functions/common/complete_profile.php
+++ functions/common/complete_profile.php
@@ -1,1 +1,22 @@
+<?php
+include_once("../customer_functions.php");
 
+$jsonData = json_decode(file_get_contents("php://input"), true);
+
+
+ $firstname = $jsonData['firstname'];
+ $lastname = $jsonData['lastname'];
+ $phonenumber = $jsonData['phonenumber'];
+ $accountType = $jsonData['accountType'];
+ $region = $jsonData['region'];
+ $town = $jsonData['town'];
+ $quarter = $jsonData['quarter'];
+
+ $result = completeProfile($firstname, $lastname, $phonenumber, $accountType, $region, $town, $quarter);
+
+ $data = array(
+    'state'=>$result,
+);
+
+
+echo json_encode($data);
