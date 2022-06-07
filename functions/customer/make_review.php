c4507f4d6abdfcd0484b08e8ffaf74685027ed04
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-07 23:54:42 +0100

    still working

--- functions/customer/make_review.php
+++ functions/customer/make_review.php
@@ -2,3 +2,18 @@
 
 $jsonData = json_decode(file_get_contents("php://input"), true);
 
+$customer_id = $jsonData['customer_id'];
+$seller_id = $jsonData['seller_id'];
+$rating = $jsonData['rating'];
+$review = $jsonData['review'];
+
+$result = makeReview($customer_id, $seller_id, $rating, $review);
+
+$data = array(
+    'state'=>$result,
+);
+
+var_dump($jsonData);
+
+
+echo json_encode($data);
