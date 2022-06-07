f1ba831ce49295bff0736252bae8fb5c4bab09ad
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-07 23:54:09 +0100

    login good

--- functions/common/login.php
+++ functions/common/login.php
@@ -1,24 +1,6 @@
 <?php
 include_once("../customer_functions.php");
 
-    // $email = $_POST['email'];
-    // $password = $_POST['password'];
-
-    // var_dump($jsonData = json_decode(file_get_contents("php://input"), true));
-   
-
-    // $result = login($email, $password);
-    // //pt the result and name in associative array to send back to front end
-
-    // $data = array(
-    //     'state'=>$result,
-    // );
-    
-
-    // echo json_encode($data);
-
- 
-
     $jsonData = json_decode(file_get_contents("php://input"), true);
 
     $email = $jsonData['email'];
