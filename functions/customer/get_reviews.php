94a6b3b3edc17aac0b14c097ce0edfb63930023b
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-08 00:47:01 +0100

    all good

--- functions/customer/get_reviews.php
+++ functions/customer/get_reviews.php
@@ -1,4 +1,17 @@
 <?php
 
+include_once("../customer_functions.php");
+
+
 $jsonData = json_decode(file_get_contents("php://input"), true);
 
+$id = $jsonData['id'];
+
+$result = getReviews($id);
+
+$data = array(
+    'state'=>$result,
+);
+
+
+echo json_encode($data);
