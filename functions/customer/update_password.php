61c03230a3a7ea4215fc3baaa0c10b2ec5190270
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-08 08:07:45 +0100

    ok

--- functions/customer/update_password.php
+++ functions/customer/update_password.php
@@ -1,4 +1,19 @@
 <?php
 
+include_once("../customer_functions.php");
+
+
 $jsonData = json_decode(file_get_contents("php://input"), true);
 
+$id = $jsonData['id'];
+$old_password = $jsonData['oldPassword'];
+$new_password = $jsonData['newPassword'];
+
+$result = updatePassword($old_password, $new_password, $id);
+
+$data = array(
+    'state'=>$result,
+);
+
+
+echo json_encode($data);
