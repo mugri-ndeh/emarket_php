eb60a078b018fa32a1625d8341b6be993ee7cfbf
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-08 08:07:19 +0100

    ok

---
+++ functions/seller/view_stores.php
@@ -0,0 +1,18 @@
+<?php
+include_once("../seller_functions.php");
+
+    $jsonData = json_decode(file_get_contents("php://input"), true);
+
+    $uid = $jsonData['seller_id'];
+
+   
+
+    $result = viewStores($uid);
+    //pt the result and name in associative array to send back to front end
+
+    $data = array(
+        'state'=>$result,
+    );
+    
+
+    echo json_encode($data);
