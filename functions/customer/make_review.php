607031f7ea82ae823adcedd151d55908de3abcd6
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-08 12:30:22 +0100

    works on postman

--- functions/customer/make_review.php
+++ functions/customer/make_review.php
@@ -6,11 +6,11 @@ include_once("../customer_functions.php");
 $jsonData = json_decode(file_get_contents("php://input"), true);
 
 $customer_id = $jsonData['customer_id'];
-$seller_id = $jsonData['seller_id'];
+$shop_id = $jsonData['shop_id'];
 $rating = $jsonData['rating'];
 $review = $jsonData['review'];
 
-$result = makeReview($customer_id, $seller_id, $rating, $review);
+$result = makeReview($customer_id, $shop_id, $rating, $review);
 
 $data = array(
     'state'=>$result,
