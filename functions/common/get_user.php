40b289f50026564914de64075f180cab78bfc1d7
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-09 17:14:41 +0100

    post for get usesr

--- functions/common/get_user.php
+++ functions/common/get_user.php
@@ -3,7 +3,7 @@ include_once("../customer_functions.php");
 
     $jsonData = json_decode(file_get_contents("php://input"), true);
 
-    $id = $jsonData['id'];
+    $id = $_POST['uid'];
 
     $result = getUser($id);
     //pt the result and name in associative array to send back to front end
