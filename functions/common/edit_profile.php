045ac116242740df8b02c4a9b2a09d36580edc14
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-09 17:14:20 +0100

    edit profile init

---
+++ functions/common/edit_profile.php
@@ -0,0 +1,16 @@
+<?php
+    $firstname = $_POST['firstName'];
+    $lastname = $_POST['lastName'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $phonenumber = $_POST['phoneNumber'];
+    $id = $_POST['uid'];
+    
+    $result  = editProfile($firstname, $lastname, $username, $email, $phonenumber, $id);
+
+    $data = array(
+        'state'=>$result,
+    );
+    
+
+    echo json_encode($data);
