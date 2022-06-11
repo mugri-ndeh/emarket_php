1f4756b92fded5d571c3e800afcaeff6e5869666
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-11 23:32:58 +0100

    hotfix

--- functions/customer/createOrder.php
+++ functions/customer/createOrder.php
@@ -6,13 +6,15 @@ include_once("../customer_functions.php");
 $jsonData = json_decode(file_get_contents("php://input"), true);
 
 
- $customer_id = $jsonData['customer_id'];
- $shop_id = $jsonData['shop_id'];
- $date = $jsonData['date'];
- $state = $jsonData['state'];
- $items =  ($jsonData['items']);
+ $customer_id = $_POST['customer_id'];
+ $shop_id = $_POST['shop_id'];
+ $date = $_POST['date'];
+ $state = $_POST['state'];
+ $items =  json_decode($_POST['items']);
+ $qty = $_POST['qty'];
+ $priceTotal = $_POST['price_total'];
 
-$result = createOrder($customer_id, $shop_id, $date, $state, $items);
+$result = createOrder($customer_id, $shop_id, $date, $state, $items, $qty, $priceTotal);
 
 $data = array(
     'state'=>$result,
