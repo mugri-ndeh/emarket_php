52ffd67af19b592bd41eca00472bd11c7ad7c8d8
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-11 23:33:35 +0100

    to be tested

--- functions/seller/add_products.php
+++ functions/seller/add_products.php
@@ -9,9 +9,27 @@ include_once("../seller_functions.php");
     $image = $_POST['image'];
     $shop_id = $_POST['shop_id'];
     $quantity = $_POST['quantity'];
+
+    $image = $_FILES['image']['name'];
+
+
+    $path = "../../uploads/shop/$name/$seller_id/shop_$shop_id/products";
+    $path1 = "uploads/shop/$name/$seller_id/shop_$shop_id/products";
+    
+    if (!is_dir($path)) {
+        mkdir($path, 0755, true);
+    }
+
+    $imgPath = "$path".$image;
+    $imgPath1 = "$path1".$image;
+
+    $tmp_name = $_FILES['image']['tmp_name'];
+
+
+    $imresult = move_uploaded_file($tmp_name, $imgPath1);
    
 
-    $result = addProducts($category_id, $name, $price, $image, $shop_id, $quantity);
+    $result = addProducts($category_id, $name, $price, $imgPath1, $shop_id, $quantity);
     //pt the result and name in associative array to send back to front end
 
     $data = array(
